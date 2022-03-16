d0b91804966d5a0d1d0b406b6265217ad25af2c6
Forster112 <forstermichael112@gmail.com>
2022-03-16 15:56:52 -0700

    added header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Auth: Miguels Main
- * Desc: Header file for all functions used in the project
- */
-
-int _putchar(char c);
 void print_alphabet(void);
