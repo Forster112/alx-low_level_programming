4d7910094737e54678ba25199d7a49a142bf46c0
Forster112 <forstermichael112@gmail.com>
2022-03-16 15:44:03 -0700

    prints lowercase alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: no return because void
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+}
