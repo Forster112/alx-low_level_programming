b2d4afa122f3a2b2d2c920ed4f2cbeea6272f6c2
Forster112 <forstermichael112@gmail.com>
2022-03-17 06:48:05 -0700

    positive int or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints if int is positive or negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
