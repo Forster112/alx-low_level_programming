26f386e4f02139cef090c94ca78de210f7d79b9f
Forster112 <forstermichael112@gmail.com>
2022-03-17 02:13:39 -0700

    is it alphabet?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - comfirms is alphabet
+ * @c: alphabet to be checked
+ *
+ * Return: Always 0 (success)
+ */
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
