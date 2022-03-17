a36b409296cc9c5db205141bd93cffe44d71c9b0
Forster112 <forstermichael112@gmail.com>
2022-03-17 09:45:33 -0700

    multiplies to digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two digits
+ * @a: digit to be multiplied to b
+ * @b: digit to be multiplied to a
+ * Return: results of a and b
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
