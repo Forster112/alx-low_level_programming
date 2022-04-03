736d4382d97546e2486bfc656d7667d1e35f6af7
Forster112 <91550261+Forster112@users.noreply.github.com>
2022-04-03 13:22:47 +0100

    strcat

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcat - concantenates two strings
+ * @dest: param pointer to a char
+ * @src: param pointer
+ * Return: return value of dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
